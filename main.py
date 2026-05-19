eaeeda874932e8734a1e7a1ff78a15427cf2f3aa
Maksym <1maksymrusanov1@gmail.com>
2026-05-19 19:36:28 +0100

    fixed logic

--- main.py
+++ main.py
@@ -8,40 +8,46 @@ load_dotenv()
 
 
 def get_repo_url() -> str | None:
-    url = input("Enter the GitHub repository URL (or press Enter to skip): ").strip()
+    url = input(
+        "Enter the GitHub repository URL (or press Enter to skip): ").strip()
     if url == "exit":
         print("Exiting the program.")
         return None
     elif not url.startswith("http") or "github.com" not in url:
         print("Invalid URL. Please enter a valid GitHub repository URL.")
-        return get_repo_url()
     return url
 
 
 def main(url: str | None = None):
+    url = get_repo_url()
+    repo_name = url.rstrip("/").split("/")[-1]
     api_key = os.getenv("GENAI_API_KEY")
     if not api_key:
         raise ValueError("GENAI_API_KEY not found in environment variables")
 
     prompt = f"""You are a professional technical writer. Generate a README.md for project on {url} .
-Follow these guidelines:
-- Use Markdown formatting consistently
-- Include: Description, Features, Installation, Usage, Contributing, License
-- Keep it concise and technically accurate"""
+        Follow these guidelines:
+        - Use Markdown formatting consistently
+        - Include: Description, Features, Installation, Usage, Contributing, License
+        - Keep it concise and technically accurate"""
 
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
-        model="gemini-3-flash-preview", contents=prompt
+        model="gemini-3.1-flash-lite", contents=prompt
     )
     if not response.text:
         raise ValueError("Empty response from API")
     else:
-        filename = "readme.md" if url else "test.md"
-        with open(filename, "w") as f:
-            f.write(response.text)
-
-    print(f"{filename} written to {Path().absolute()}")
+        if url:
+            filename = "README.md"
+        else:
+            raise ValueError("No URL provided, cannot generate README.md")
+    path = Path(f"~/projects/readmes/{repo_name}").expanduser()
+    path.mkdir(parents=True, exist_ok=True)
+    with open(path / filename, "w") as f:
+        f.write(response.text)
+    print(f"{filename} written to {path/filename}")
 
 
 if __name__ == "__main__":
-    main(get_repo_url())
+    main()
